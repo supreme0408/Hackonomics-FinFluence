77fa16fff7997f29a6596af4d22ff25667ff33c1
Ramanuj <osupreme0408@gmail.com>
2024-12-27 23:09:15 +0530

    minor changes

--- backend/main.py
+++ backend/main.py
@@ -43,8 +43,3 @@ def handle_business_scenario_request(data: BusinessScenarioRequest):
         return handle_business_scenario(groq,data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing request: {str(e)}")
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
